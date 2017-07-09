0f4135391edeedbd26f40e0d9784088c13d9c245
Fabian Geiselhart <me@f4814n.de>
2017-07-09 14:08:23 +0200

    Added motorDir(m) and motorSpeed(m) macros

--- bits.h
+++ bits.h
@@ -2,7 +2,7 @@
 #define BITS_H_
 
 #define setBit(reg, bit) (reg) |= (1 << (bit))
-#define unsetBit(reg, pit) (reg) &= ~(1 << (bit))
+#define unsetBit(reg, bit) (reg) &= ~(1 << (bit))
 #define checkBit(reg, bit) (((reg) & (1 << (bit))) != 0) ? true : false
 
 #endif
--- motors.h
+++ motors.h
@@ -2,6 +2,9 @@
 #define MOTORS_H_
 #include <stdbool.h>
 
+#define motorSpeed(motor) (motor == 0) ? OCR1B : (!motorDir(motor) ? 255 - OCR2A : OCR2A)
+#define motorDir(motor) (motor == 0) ? PORTB1 : PORTB0
+
 void motor_setup(uint8_t motor);
 void motor_update(uint8_t motor, bool direction, uint8_t speed);
 
