9a52f22a5ee0a4b7db81cd2e82e94691ab7f6bbb
Fabian Geiselhart <me@f4814n.de>
2017-07-09 18:41:01 +0200

    Fix bug

--- motors.h
+++ motors.h
@@ -2,7 +2,7 @@
 #define MOTORS_H_
 #include <stdbool.h>
 
-#define motorSpeed(motor) (motor == 0) ? OCR1B : (!motorDir(motor) ? 255 - OCR2A : OCR2A)
+#define motorSpeed(motor) (motor == 0) ? OCR1B : OCR2A
 #define motorDir(motor) (motor == 0) ? PORTB1 : PORTB0
 
 void motor_setup(uint8_t motor);
