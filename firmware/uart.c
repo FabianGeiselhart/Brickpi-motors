b63039375a5bbfe9d0ed60a0d088ae653aeced3c
Fabian Geiselhart <me@f4814n.de>
2017-07-15 09:27:04 +0200

    Added uart functionality

---
+++ firmware/uart.c
@@ -0,0 +1,78 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdbool.h>
+#include "bits.h"
+#include "motors.h"
+#include "uart.h"
+#include <avr/interrupt.h>
+
+/* Protocol:
+ M/S: 2 7-bit bytes
+ 0. Number(Not yet)
+ 1. Destination (Not yet)
+
+ 2. Target (Motor 0 or 1)
+ 3. 4. Action (0 = Set speed, 1 = Set direction,
+               2 = request encoder, (3 = Get config))
+ 5. Direction
+ 0..7. Speed (arbitrary if mode != 0)
+ */
+
+// function to send data
+void uart_transmit(unsigned char data)
+{
+    while (!( UCSR0A & (1<<UDRE0)));                // wait while register is free
+    UDR0 = data;                                   // load data in the register
+}
+
+void uart_flush( void ) {
+        unsigned char dummy;
+        while ( UCSR0A & (1<<RXC0) ) dummy = UDR0;
+}
+
+ISR(USART_RX_vect) { // React on sent data
+    GPIOR0++;
+    if (GPIOR0 % 4 == 0) {
+        GPIOR1 = UDR0;
+    }
+    uart_flush();
+}
+
+void uart_motor(uint8_t data) {
+    static bool speed = false;
+    static uint8_t motor;
+ 
+    uart_transmit(data);
+    uart_transmit(motor);
+    uart_transmit('-');
+    if (speed) {
+        motor_update(motor, motorDir(motor), data);
+        speed = false;
+        return;
+    } else {
+        motor = checkBit(data, 2);
+    }
+
+    if (!checkBit(data, 3) && !checkBit(data, 4)) { // Set Speed
+        speed = true;
+        uart_transmit('s');
+    } else if (checkBit(data, 3) && !checkBit(data, 4)) { // Set direction
+        motor_update(motor, checkBit(data, 5), motorSpeed(motor));
+        uart_transmit('d');
+    }
+}
+
+void uart_setup() {
+    UBRR0H = (BAUDRATE >> 8);
+    UBRR0L = BAUDRATE;
+
+    // Set 8-Bit mode
+    setBit(UCSR0C, UCSZ01);
+    setBit(UCSR0C, UCSZ00);
+
+    setBit(UCSR0B, RXEN0); // Receiver enable
+    setBit(UCSR0B, TXEN0); // Transmitter enable
+    
+    setBit(UCSR0B, RXCIE0); // Enable RX Interrupt
+    sei(); // Enable global interrupts
+}
---
+++ firmware/uart.h
@@ -0,0 +1,11 @@
+#ifndef UART_H_
+#define UART_H_
+
+#define BAUD 9600// define baud
+#define BAUDRATE ((F_CPU)/(BAUD*16UL)-1) // set baud rate value for UBRR
+
+void uart_transmit(unsigned char data);
+void uart_motor(uint8_t data);
+void uart_setup();
+
+#endif
