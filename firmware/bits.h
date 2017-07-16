e7c73377b8f0a35e3231f887668467d17f912079
Fabian Geiselhart <me@f4814n.de>
2017-07-16 14:21:43 +0200

    Deleted checkBit, use bit_is_set instead

--- firmware/bits.h
+++ firmware/bits.h
@@ -3,6 +3,5 @@
 
 #define setBit(reg, bit) (reg) |= (1 << (bit))
 #define unsetBit(reg, bit) (reg) &= ~(1 << (bit))
-#define checkBit(reg, bit) (((reg) & (1 << (bit))) != 0) ? true : false
 
 #endif
