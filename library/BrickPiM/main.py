ad53a667bd8df55628d5d61ba34dd7c4837e2fac
Robotics team <me@f4814n.de>
2017-07-20 13:22:32 +0000

    Send 1 byte instead of 4 bytes

--- library/BrickPiM/main.py
+++ library/BrickPiM/main.py
@@ -6,9 +6,6 @@ from struct import pack
 from time import sleep
 import RPi.GPIO as GPIO
 
-# pack(">I", BYTE)
-
-print("Imports abgeschlossen")
 
 class BrickPi:
 
@@ -16,19 +13,11 @@ class BrickPi:
         self.ser = serial.Serial("/dev/ttyAMA0")
         self.ser.baudrate = 9600
 
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(24, GPIO.OUT)
-        print("Start pressing reset")
-        GPIO.output(24, GPIO.HIGH)
-        sleep(.2)
-        GPIO.output(24, GPIO.LOW)
-        print("Stop pressing reset")
-
     def updateSpeed(self, motor, speed):
         tmp = 0
         tmp |= (motor << 2)
-        self.ser.write(pack(">I", tmp))
-        self.ser.write(pack(">I", speed))
+        self.ser.write(pack("!B", tmp))
+        self.ser.write(pack("!B", speed))
         sleep(.1)
 
     def updateDirection(self, motor, direction):
@@ -36,17 +25,9 @@ class BrickPi:
         tmp |= (motor << 2)
         tmp |= (1 << 3)
         tmp |= (direction << 5)
-        self.ser.write(pack(">I", tmp))
+        self.ser.write(pack("!B", tmp))
         sleep(.1)
 
     def stop(self):
         self.updateSpeed(0, 0)
         self.updateSpeed(1, 0)
-
-if __name__ == "__main__":
-    b = BrickPi()
-    b.updateSpeed(1, 100)
-    b.updateSpeed(0, 100)
-    sleep(2)
-    b.updateSpeed(0, 0)
-    b.updateSpeed(1, 0)
