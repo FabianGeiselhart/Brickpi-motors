cb70266a800393219d4645b1015f6664da918baa
Fabian Geiselhart <me@f4814n.de>
2017-07-21 15:19:38 +0200

    Round Baudrate

--- firmware/uart.h
+++ firmware/uart.h
@@ -2,7 +2,7 @@
 #define UART_H_
 
 #define BAUD 9600// define baud
-#define BAUDRATE ((F_CPU)/(BAUD*16UL)-1) // set baud rate value for UBRR
+#define BAUDRATE ((F_CPU)/(BAUD*16)-1) // set baud rate value for UBRR
 
 void uart_transmit(unsigned char data);
 void uart_motor(uint8_t data);
